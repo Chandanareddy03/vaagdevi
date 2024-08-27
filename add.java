cf6c4c6651b6b7407dceb651f5684d0c18827461
Nomula Chandana <chandanareddynomula123@gmail.com>
2024-08-27 12:25:34 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,11 @@
+
+class addwithoutop {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 3;
+        int sum= (a-(-b));
+        System.out.println("sum:" +sum);
+    }
+}
+
+
